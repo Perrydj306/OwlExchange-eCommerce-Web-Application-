324e3cb6fd934b24349cb5776d4718f5385b3719
Perrydj306 <perrydj306@gmail.com>
2025-09-23 17:00:00 -0400

    First attempt to make the website look like our wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import AdminPage from "./AdminPage";
 import UserDashboard from "./UserDashboard";
+import LandingPage from "./LandingPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} /> {/* Landing Page*/}
+        <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminPage />} /> {/* ✅ Admin route */}
         <Route path="/dashboard" element={<UserDashboard />} /> {/* ✅ User page */}
       </Routes>
